d6d0eb859a2057695a2b671b6f85d13eace3c057
Josué Silva <josueluzsilva.01@gmail.com>
2023-08-16 10:06:22 -0300

    tocaSom

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,5 +1,12 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < 9) {
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador++;
+}
