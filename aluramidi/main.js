7810ee3d666534b9ca77c2d3e891f7670ead62e4
Josué <josueluzsilva.01@gmail.com>
2023-08-16 18:06:56 -0300

    refinações em js no projeto

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -4,9 +4,24 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < 9) {
-    listaDeTeclas[contador].onclick = tocaSom;
-    contador++;
-}
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}       
