73e30ecbd032683ce3370b513a70038774b79652
Josué Silva <josueluzsilva.01@gmail.com>
2023-08-24 10:03:50 -0300

    completando campos

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,16 +1,29 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        var consultarCEP = await fetch("https://viacep.com.br/ws/01001250/json/");
+        var consultarCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultarCEP.json();
+
+        let endereco = document.querySelector("#endereco");
+        let cidade = document.querySelector("#cidade");
+        let estado = document.querySelector("#estado");
+        let bairro = document.querySelector("#bairro");
+
+        endereco.value = consultaCEPConvertida.logradouro;
+        cidade.value = consultaCEPConvertida.localidade;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
 
         if(consultaCEPConvertida.erro) {
             throw Error("CEP não existente!");
         }
     
         console.log(consultaCEPConvertida);
+
+        return consultaCEPConvertida;
     } catch(erro) {
         console.log(erro);
     }
 }
 
-buscaEndereco();
+let cep = document.querySelector("#cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
